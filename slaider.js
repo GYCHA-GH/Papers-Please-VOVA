b25654e7c4419f0da3200a153721e6f90290ab25
Живетин Роман <124517815+GYCHA-GH@users.noreply.github.com>
2024-04-12 14:14:11 +0200

    eew

--- slaider.js
+++ slaider.js
@@ -1,173 +1,21 @@
-function Sim(sldrId) {
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
 
-	let id = document.getElementById(sldrId);
-	if(id) {
-		this.sldrRoot = id
-	}
-	else {
-		this.sldrRoot = document.querySelector('.sim-slider')
-	};
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 900;
+    if (offset > 10800) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
 
-	// Carousel objects
-	this.sldrList = this.sldrRoot.querySelector('.sim-slider-list');
-	this.sldrElements = this.sldrList.querySelectorAll('.sim-slider-element');
-	this.sldrElemFirst = this.sldrList.querySelector('.sim-slider-element');
-	this.leftArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-left');
-	this.rightArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-right');
-	this.indicatorDots = this.sldrRoot.querySelector('div.sim-slider-dots');
-
-	// Initialization
-	this.options = Sim.defaults;
-	Sim.initialize(this)
-};
-
-Sim.defaults = {
-
-	// Default options for the carousel
-	loop: true,     // Бесконечное зацикливание слайдера
-	auto: true,     // Автоматическое пролистывание
-	interval: 5000, // Интервал между пролистыванием элементов (мс)
-	arrows: true,   // Пролистывание стрелками
-	dots: true      // Индикаторные точки
-};
-
-Sim.prototype.elemPrev = function(num) {
-	num = num || 1;
-
-	let prevElement = this.currentElement;
-	this.currentElement -= num;
-	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
-
-	if(!this.options.loop) {
-		if(this.currentElement == 0) {
-			this.leftArrow.style.display = 'none'
-		};
-		this.rightArrow.style.display = 'block'
-	};
-	
-	this.sldrElements[this.currentElement].style.opacity = '1';
-	this.sldrElements[prevElement].style.opacity = '0';
-
-	if(this.options.dots) {
-		this.dotOn(prevElement); this.dotOff(this.currentElement)
-	}
-};
-
-Sim.prototype.elemNext = function(num) {
-	num = num || 1;
-	
-	let prevElement = this.currentElement;
-	this.currentElement += num;
-	if(this.currentElement >= this.elemCount) this.currentElement = 0;
-
-	if(!this.options.loop) {
-		if(this.currentElement == this.elemCount-1) {
-			this.rightArrow.style.display = 'none'
-		};
-		this.leftArrow.style.display = 'block'
-	};
-
-	this.sldrElements[this.currentElement].style.opacity = '1';
-	this.sldrElements[prevElement].style.opacity = '0';
-
-	if(this.options.dots) {
-		this.dotOn(prevElement); this.dotOff(this.currentElement)
-	}
-};
-
-Sim.prototype.dotOn = function(num) {
-	this.indicatorDotsAll[num].style.cssText = 'background-color:#BBB; cursor:pointer;'
-};
-
-Sim.prototype.dotOff = function(num) {
-	this.indicatorDotsAll[num].style.cssText = 'background-color:#556; cursor:default;'
-};
-
-Sim.initialize = function(that) {
-
-	// Constants
-	that.elemCount = that.sldrElements.length; // Количество элементов
-
-	// Variables
-	that.currentElement = 0;
-	let bgTime = getTime();
-
-	// Functions
-	function getTime() {
-		return new Date().getTime();
-	};
-	function setAutoScroll() {
-		that.autoScroll = setInterval(function() {
-			let fnTime = getTime();
-			if(fnTime - bgTime + 10 > that.options.interval) {
-				bgTime = fnTime; that.elemNext()
-			}
-		}, that.options.interval)
-	};
-
-	// Start initialization
-	if(that.elemCount <= 1) {   // Отключить навигацию
-		that.options.auto = false; that.options.arrows = false; that.options.dots = false;
-		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
-	};
-	if(that.elemCount >= 1) {   // показать первый элемент
-		that.sldrElemFirst.style.opacity = '1';
-	};
-
-	if(!that.options.loop) {
-		that.leftArrow.style.display = 'none';  // отключить левую стрелку
-		that.options.auto = false; // отключить автопркрутку
-	}
-	else if(that.options.auto) {   // инициализация автопрокруки
-		setAutoScroll();
-		// Остановка прокрутки при наведении мыши на элемент
-		that.sldrList.addEventListener('mouseenter', function() {clearInterval(that.autoScroll)}, false);
-		that.sldrList.addEventListener('mouseleave', setAutoScroll, false)
-	};
-
-	if(that.options.arrows) {
-		that.leftArrow.addEventListener('click', function() {
-			let fnTime = getTime();
-			if(fnTime - bgTime > 1000) {
-				bgTime = fnTime; that.elemPrev()
-			}
-		}, false);
-		that.rightArrow.addEventListener('click', function() {
-			let fnTime = getTime();
-			if(fnTime - bgTime > 1000) {
-				bgTime = fnTime; that.elemNext()
-			}
-		}, false)
-	}
-	else {
-		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
-	};
-	if(that.options.dots) {
-		let sum = '', diffNum;
-		for(let i=0; i<that.elemCount; i++) {
-			sum += '<span class="sim-dot"></span>'
-		};
-		that.indicatorDots.innerHTML = sum;
-		that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.sim-dot');
-		for(let n=0; n<that.elemCount; n++) {
-			that.indicatorDotsAll[n].addEventListener('click', function() {
-				diffNum = Math.abs(n - that.currentElement);
-				if(n < that.currentElement) {
-					bgTime = getTime(); that.elemPrev(diffNum)
-				}
-				else if(n > that.currentElement) {
-					bgTime = getTime(); that.elemNext(diffNum)
-				}
-				
-			}, false)
-		};
-		that.dotOff(0);  
-		for(let i=1; i<that.elemCount; i++) {
-			that.dotOn(i)
-		}
-	}
-};
-new Sim();
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 900;
+    if (offset < 0) {
+        offset = 900;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
 
 const button1 = document.getElementById('button1');
 const inputField = document.getElementById('inputField');
@@ -215,16 +63,6 @@ button3.addEventListener('blur', function() {
 });
 
 
-function validateForm() {
-    var email = document.getElementById('email').value;
-    if (!email.includes('@')) {
-        alert('Пожалуйста, введите корректный email');
-        return false;
-    }
-    return true;
-}
-
-
 
 //ебейшое задание от вовы ебать 
 const changeStyleBtn = document.getElementById('changeStyleBtn');
@@ -238,15 +76,9 @@ resetStyleBtn.addEventListener('click', function() {
     elementToStyle.classList = 'passvord';
 });
 
-
-
-burgerOpen.addEventListener("click", ()=> {
+burgerOpen.addEventListener("click", function() {
     burgerMenu.style.right="0%"
-    burgerBack.style.opacity="1"
-    burgerBack.style.visibility="visible"
-})
-burgerClose.addEventListener("click", ()=> {
-    burgerMenu.style.right="-80%"
-    burgerBack.style.opacity="1"
-    burgerBack.style.visibility="visible"
-})
+});
+burgerClose.addEventListener("click", function() {
+    burgerMenu.style.right="-30%"
+});
